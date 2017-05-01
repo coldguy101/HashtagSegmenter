d37087db49d45569713a103e4b5cb92c5cdc3721
Sean Lavoie <sean@n2c7.com>
2017-05-01 16:43:27 -0500

    Adding n-gram files, and stuff

--- Segmentation.py
+++ Segmentation.py
@@ -1,28 +1,89 @@
+import math
 from Trie import Trie
 
-trie = Trie()
 
-with open("words.txt") as f:
-    content = f.readlines()
+class OneGramDist(dict):
+    def __init__(self, filename):
+        self.gramCount = 0
 
-with open("slang.txt") as s:
-    content2 = s.readlines()
+        for line in open(filename):
+            (word, count) = line[:-1].split('\t')
+            self[word] = int(count)
+            self.gramCount += self[word]
 
-content = [x.strip().lower() for x in content]
-content2 = [x.strip().lower() for x in content2]
+    def __call__(self, key):
+        if key in self:
+            return float(self[key]) / self.gramCount
+        else:
+            return 1.0 / (self.gramCount * 10 ** (len(key) - 2))
 
-content.extend(content2)
 
-trie.make_trie(content)
+singleWordProb = OneGramDist('one-grams.txt')
 
-print trie.in_trie("race")
-print trie.in_trie("racecars")
-print trie.in_trie("racecar")
-print trie.in_trie("ultra")
-print trie.in_trie("fine")
-print trie.in_trie("boody")
-print trie.in_trie("ass")
-print trie.in_trie("love")
-print trie.in_trie("life")
-print trie.in_trie("lyfe")
-print trie.in_trie("lmao")
+
+def wordSeqFitness(words):
+    prob = sum(math.log10(singleWordProb(w)) for w in words)
+    print "Words: " + str(words) + " prob: " + str(prob)
+    return prob
+
+
+def segment(word):
+    if not word:
+        return []
+
+    allSegmentations = [[first] + segment(rest) for (first, rest) in splitPairs(word)]
+
+    # allSegmentations = []
+    # for (first, rest) in splitPairs(word):
+    #     ex = [first] + segment(rest)
+    #     print ex
+    #     allSegmentations.append(ex)
+
+    return max(allSegmentations, key=wordSeqFitness)
+
+
+def segmentWithProb(word):
+    segmented = segment(word)
+    return (wordSeqFitness(segmented), segmented)
+
+
+def splitPairs(word, maxLen=20):
+    return [(word[:i + 1], word[i + 1:]) for i in range(max(len(word), maxLen))]
+
+
+def segmentHashTag(hashtag):
+    if '#' not in hashtag:
+        return "not a hashtag"
+    hashtag = hashtag.lower()  # change to lower case
+    return segment(hashtag[1:])
+
+print segmentHashTag("#hellotherebud")
+
+# from Trie import Trie
+#
+# trie = Trie()
+#
+# with open("words.txt") as f:
+#     content = f.readlines()
+#
+# with open("slang.txt") as s:
+#     content2 = s.readlines()
+#
+# content = [x.strip().lower() for x in content]
+# content2 = [x.strip().lower() for x in content2]
+#
+# content.extend(content2)
+#
+# trie.make_trie(content)
+#
+# possibleWords = []
+#
+# example = "#iloveyou"
+#
+# # Start at 1 to skip the hashtag
+# for i in range(1, len(example) + 1):
+#     for j in range(i + 1, len(example) + 1):
+#         if trie.in_trie(example[i:j]):
+#             possibleWords.append(example[i:j])
+#
+# print possibleWords
